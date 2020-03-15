4849a13ce30133c2dc7ce56d28e88c32830a4628
AxE33 <alexander.eret@stud.hshl.de>
2020-03-15 21:12:52 +0100

    testB Update

---
+++ HTMLTest/app/src/main/java/com/example/htmltest/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.htmltest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = (WebView)findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebChromeClient(new WebChromeClient());
+        webView.loadUrl("file:///android_asset/sample.html");
+    }
+}
