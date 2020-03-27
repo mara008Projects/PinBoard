d9ed854804fbd334b712046515fd8a470218899e
AxE33 <alexander.eret@stud.hshl.de>
2020-03-27 15:08:47 +0100

    Update 27-03
    Kleinigkeiten umorganisiert

--- HTMLTest/app/build/intermediates/merged_assets/debug/out/app.js
+++ HTMLTest/app/build/intermediates/merged_assets/debug/out/app.js
@@ -1,3 +1,10 @@
+var lName = "London";
+
 function sayhello(){
 alert('hi, ' + document.getElementById('name').value + '!');
+lName = document.getElementById('name').value;
+}
+
+function setTextLondon(){
+
 }
