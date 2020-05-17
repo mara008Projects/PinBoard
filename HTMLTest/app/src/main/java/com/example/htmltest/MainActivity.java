7c28fe375ef8cc31b40fa1928d4c0169342e438b
Mandy <mandy.radomski@eurosoft.net>
2020-05-17 23:10:43 +0200

    fixURlexception
    verlinkung auf html seiten geht jetzt

--- HTMLTest/app/src/main/java/com/example/htmltest/MainActivity.java
+++ HTMLTest/app/src/main/java/com/example/htmltest/MainActivity.java
@@ -1,20 +1,34 @@
 package com.example.htmltest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.webkit.WebChromeClient;
 import android.webkit.WebView;
 
-public class MainActivity extends AppCompatActivity {
+import static android.app.PendingIntent.getActivity;
+
+public class MainActivity extends AppCompatActivity  {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         WebView webView = (WebView)findViewById(R.id.webView);
+
         webView.getSettings().setJavaScriptEnabled(true);
-        webView.setWebChromeClient(new WebChromeClient());
-        webView.loadUrl("file:///android_asset/index.html");
+
+        webView.loadUrl("file:///android_asset/pinboardHtmlFiles/index.html");
+
+           /*  10楼  Just paste the below code in activity onCreate()
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder(); StrictMode.setVmPolicy(builder.build());
+        https://www.koubeiblog.com/inteqa/38200282.html  */
+
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder(); StrictMode.setVmPolicy(builder.build());
     }
+
 }
