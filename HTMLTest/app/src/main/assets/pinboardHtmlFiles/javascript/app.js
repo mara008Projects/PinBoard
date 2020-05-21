c4945997751bb4157d8c5f950bdccf687af97533
Mandy <mandy.radomski@eurosoft.net>
2020-05-21 23:55:47 +0200

    startpage

---
+++ HTMLTest/app/src/main/assets/pinboardHtmlFiles/javascript/app.js
@@ -0,0 +1,12 @@
+var lName = "London";
+
+function sayhello(){
+alert('hi, ' + document.getElementById('name').value + '!');
+lName = document.getElementById('name').value;
+}
+
+function setTextLondon(){
+
+}
+
+
---
+++ HTMLTest/app/src/main/assets/pinboardHtmlFiles/javascript/index-Table-script.js
@@ -0,0 +1,63 @@
+/* index-Table-script */
+
+/* funktionen Drag&Drop */
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+  ev.preventDefault();
+  var data = ev.dataTransfer.getData("text");
+  ev.target.appendChild(document.getElementById(data));
+  }
+
+/* funktionen PopUp */
+function input(){
+//	alert ("New angeklickt");
+	if (document.getElementById("pContainer").style.display == "block"){
+		document.getElementById("pContainer").style.display = "none";
+	} else {
+		document.getElementById("pContainer").style.display = "block";
+	}
+}
+
+// https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_todo 
+
+function newElement(){
+//	alert ("Add angeklickt");
+	var name = document.getElementById("nameInput").value;
+	
+	if (name === '') {
+		alert ("Ungültiger Name!");
+	} else  {
+	//	document.getElementById("myTable").appendChild(th);
+		input(); //schließt PopUp 
+		
+		placeOnTable(name);	
+	}
+	 
+}
+
+
+function placeOnTable(name){
+	var nDiv = document.createElement('div');
+	nDiv.className = "note"
+	nDiv.setAttribute( 'draggable', true);
+	nDiv.setAttribute('ondragstart',"drag(event)");
+	nDiv.id = "drag4";
+	nDiv.setAttribute('onclick',"location.href='prototype01.html';");
+					
+	nDiv.innerHTML = name;
+	
+	document.getElementById('t-11').appendChild( nDiv );
+	
+	
+	
+}//*/
+
+
+
